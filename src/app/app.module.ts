8ee56edfb993c7462979128b85536fc705ff5a35
Mest <mbahstanley77@yahoo.com>
2019-01-23 14:55:19 +0100

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { CategoryComponent } from './category/category.component';
 import { RoomComponent } from './room/room.component';
+import { NgxUploaderModule } from 'ngx-uploader';
 //import { UploadOutput, UploadInput, UploadFile, humanizeBytes, UploaderOptions } from 'ngx-uploader';
 
 
@@ -46,6 +47,7 @@ const route=[{
     FormsModule,
    HttpModule,
    ReactiveFormsModule,
+   NgxUploaderModule,
    RouterModule.forRoot(route)
   ],
   providers: [
